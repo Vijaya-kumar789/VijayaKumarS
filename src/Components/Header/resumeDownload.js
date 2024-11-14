da2a51a1fe413d950fa4eaf40ed13259bb072fcc
vijaya-kumar789 <vijaykumaryoyo8@gmail.com>
2024-11-14 14:26:54 +0530

    changes in resume link

--- src/Components/Header/resumeDownload.js
+++ src/Components/Header/resumeDownload.js
@@ -1,9 +1,8 @@
 
  export const handleDownload = () => {
-  const pdfUrl = 'https://drive.google.com/file/d/1PStZgogg9Lx6RFG8pk62G34vcE2LJSWS/view?usp=sharing'; // Replace with your actual Google Drive link
+  const pdfUrl = 'https://drive.google.com/uc?export=download&id=1PStZgogg9Lx6RFG8pk62G34vcE2LJSWS'; // Replace with your actual Google Drive link
     const link = document.createElement('a');
     link.href = pdfUrl;
     link.download = 'resume.pdf'; // Optional: default file name after download
     link.click();
   };
- 
